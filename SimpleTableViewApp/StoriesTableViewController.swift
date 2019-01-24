54567746b8f741cc31eb9c03e094b09a120b568c
AlejoKA <a.kapranov@gmail.com>
2019-01-24 17:04:34 +0300

    Сократил количество секций до 1 Установил количество ячеек в соответствии с размером массива Присвоил заголовкам ячеек значения из массива

--- SimpleTableViewApp/StoriesTableViewController.swift
+++ SimpleTableViewApp/StoriesTableViewController.swift
@@ -29,36 +29,32 @@ class StoriesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         
-        return 3
+        return 1
     }
 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 5
+        return headlines.count
+        
     }
 
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        return "Section \(section)"
-    }
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//
+//        return "Section \(section)"
+//    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        cell.textLabel?.text = headlines[indexPath.row].title
 
         return cell
     }
